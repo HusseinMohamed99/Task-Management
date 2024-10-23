541683113baa8280bb6ffce81b5fa2a0de7c6430
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-23 16:37:29 +0300

    Update dependencies and fix localization imports
    - Updated various dependencies in `pubspec.lock` to their latest versions, including `_fe_analyzer_shared`, `analyzer`, `cloud_firestore`, `firebase_core`, `intl`, `logging`, `mime`, `path_provider_android`, `shared_preferences`, `test_api`, `uuid`, and others.
    - Fixed localization imports in `messages_all.dart` and `l10n.dart` to use the correct package paths.
    - Changed the default language in `settings_provider.dart` from 'en' to 'ar'.
    - Updated the Dart and Flutter SDK constraints in `pubspec.lock` to reflect the latest supported versions.

--- lib/shared/providers/settings_provider.dart
+++ lib/shared/providers/settings_provider.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
   ThemeMode currentTheme = ThemeMode.light;
-  String currentLanguage = 'en';
+  String currentLanguage = 'ar';
 
   void changeLanguage(String newLanguage) async {
     final pref = await SharedPreferences.getInstance();
